9fce563b26a5f8133504d6f0c8e23ed21fbdf854
Raffaele Rialdi <raffaeler@vevy.com>
2026-03-13 12:38:18 +0100

    Adding lavinmq

--- RestReactAspire.AppHost/AppHost.cs
+++ RestReactAspire.AppHost/AppHost.cs
@@ -1,5 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var lavinMq = builder.AddContainer("lavinmq", "cloudamqp/lavinmq")
+    .WithEndpoint(name: "amqp", port: 5672, targetPort: 5672)
+    .WithEndpoint(name: "management", port: 15672, targetPort: 15672)
+    .WithBindMount(@"H:\VMs\ContainerData\lavinmq", "/tmp/amqp");
+
 var server = builder.AddProject<Projects.RestReactAspire_Server>("server")
     .WithHttpHealthCheck("/health")
     .WithExternalHttpEndpoints();
